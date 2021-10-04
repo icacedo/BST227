48dd33101d27ba7e06a77974a32525de25b0463d
ismael <icacedo@ucdavis.edu>
2021-10-04 16:17:49 -0700

    start

---
+++ make_matrix.py
@@ -0,0 +1,5 @@
+'''
+Code that takes in a file of sequences and produces the matrix X_{ijk} that was
+described in class. Assume the text file input is a set of N sequences (one per
+line, all the same length, with characters A, C, G and T). 
+'''

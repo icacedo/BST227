b1ce7d381fac1acd5445a6d2b4f046a3fdabb8e0
ismael <icacedo@ucdavis.edu>
2021-10-13 21:26:24 -0700

    update

--- E.py
+++ E.py
@@ -32,6 +32,9 @@ def lambdas(decimal):
 	lambda1 = round(1 - lambda0, decimal)
 	return lambda0, lambda1
 
+# do these need to be in a loop?
+# to calculate posteriors for different values of lambda and psi?
+# only need to calculate posterior 4 times, but for different lambda and psi values?
 tup_l = lambdas(3)
 lambda0 = tup_l[0]
 lambda1 = tup_l[1]
@@ -42,7 +45,11 @@ psi0 = tup_p[0]
 psi1 = tup_p[1]
 print(psi0, psi1)
 
-'''		
+# L is the total number of a particular nucleotide that was seen?
+# if L = 0, nothing was seen, what does L = -1 mean? L = infinity?
+# but L can only be 0 or 1?
+
+'''	
 for sequence in mm.make_matrix(sys.argv[1]):
 	for position in seq:
 		print(position)
